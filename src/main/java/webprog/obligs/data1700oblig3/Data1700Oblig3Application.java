dfcf2985040cf26f7d0834b864dae4e15f39a412
selmadyrlie <sejoh0292@oslomet.no>
2024-12-14 10:07:31 +0100

    Initial commit

---
+++ src/main/java/webprog/obligs/data1700oblig3/Data1700Oblig3Application.java
@@ -0,0 +1,13 @@
+package webprog.obligs.data1700oblig3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Oblig3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data1700Oblig3Application.class, args);
+	}
+
+}
---
+++ src/test/java/webprog/obligs/data1700oblig3/Data1700Oblig3ApplicationTests.java
@@ -0,0 +1,13 @@
+package webprog.obligs.data1700oblig3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700Oblig3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
